85ceb5a53f42cc196ffb0bc740aac60c0bbc1ec8
Cian Donegan <ciandonegan@yahoo.ie>
2021-11-17 04:04:49 +0000

    Few adds populate database need

--- SDEV3CA1/settings.py
+++ SDEV3CA1/settings.py
@@ -137,6 +137,10 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
--- SDEV3CA1/urls.py
+++ SDEV3CA1/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('news/',include('news.urls')),
     path('',HomePageView.as_view(),name='home')
-] +static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] +static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
